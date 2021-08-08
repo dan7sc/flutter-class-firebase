e59627045cbf61758c0e97c9ffd387295434e868
dan7sc <su7cheng@gmail.com>
2021-08-08 17:42:02 -0300

    Add routes for pages

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,6 +1,7 @@
+import 'package:class_firebase/modules/home/home_page.dart';
 import 'package:class_firebase/modules/login/login_page.dart';
+import 'package:class_firebase/modules/splash/splash_page.dart';
 import 'package:flutter/material.dart';
-
 
 class AppWidget extends StatelessWidget {
   @override
@@ -10,7 +11,12 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (_) => SplashPage(),
+        "/login": (_) => LoginPage(),
+        "/home": (_) => HomePage(),
+      },
     );
   }
 }
